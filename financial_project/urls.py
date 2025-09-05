2c53d4b688cb1c0ef9a99fe660a7c51ee4b677e5
Abhijit <444abhiupasani@gmail.com>
2025-09-06 00:11:23 +0530

    Fix database configuration for Heroku

--- financial_project/urls.py
+++ financial_project/urls.py
@@ -19,7 +19,10 @@ from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 schema_view = get_schema_view(
     openapi.Info(
         title="Financial Services API",
@@ -35,7 +38,8 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),  # Django admin
-
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     # Your app's URLs
     path('', include('transactions.urls')),
 
